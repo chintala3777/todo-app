c36213a00c806155b6192681e3efb1c62a0c172f
chintala3777 <chintala.chaitanya@yahoo.com>
2023-02-18 02:24:13 +0530

    completed loic for todo

--- web.py
+++ web.py
@@ -15,7 +15,15 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.get_write_file(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label=" ", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+st.text_input(label=" ", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.session_state
